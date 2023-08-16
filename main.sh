1ed95501b585a2baf7a5fede7a67e42a4410a82c
Gabrielle Liu <gabriel814@outlook.com>
2023-08-16 10:03:09 +0800

    0816_morning

--- main.sh
+++ main.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 source globalVariable.sh
-source commonFunc.sh
 
 #config_build
 #echo helloworld
--- rebootTest/Setup_rclocalService.sh
+++ rebootTest/Setup_rclocalService.sh
@@ -1,33 +1,6 @@
 #!/bin/sh
-# Setup_OVSS_rc.local
+# Setup rc-local.service
 # Edit Date=20230815
-
-cat /dev/null > /var/log/messages
-dmesg -C
-#ipmitool sel clear
-gsys eventlog clear
-
-ifconfig usb0 10.0.1.70 netmask 255.255.255.0
-
-dos2unix reboot_configcheck.sh
-dos2unix Total_TestCountProgressBar.sh
-dos2unix Check_dmesg.sh
-dos2unix rc.local
-
-chmod 777 *
-
-sshpass -p 0penBmc scp Power_on.sh off.txt root@10.0.1.69:/
-
-cp reboot_configcheck.sh /
-cp Total_TestCountProgressBar.sh /
-cp ampereone_bmc_sel.plx /
-cp Check_dmesg.sh /
-
-cd /
-
-./reboot_configcheck.sh > Configcheck_Baseline.txt
-
-cd DC_Clean/
 
 echo [Unit] > /etc/systemd/system/rc-local.service
 echo Description=/etc/rc.local Compatibility  >> /etc/systemd/system/rc-local.service
@@ -54,5 +27,8 @@ sudo chmod +x /etc/rc.local
 sudo systemctl stop rc-local.service
 systemctl daemon-reload
 sudo systemctl enable rc-local
-timeout 10 systemctl start rc-local.service
+timeout 60 systemctl start rc-local.service
 
+
+source ../lib/createConfigLog.sh
+createConfigLog
